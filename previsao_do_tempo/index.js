c9bb2fbbd9d5a34a2f4a9b45c2121eecfe47937c
Pablo Santos <opablosantos@gmail.com>
2024-03-20 17:41:47 -0300

    test(ap01-js): encerra a apostila

--- previsao_do_tempo/index.js
+++ previsao_do_tempo/index.js
@@ -16,10 +16,7 @@ const url = `${URL_BASE}?q=${Q}&units=${UNITS}&lang=${LANGUAGE}&cnt=${CNT}&appid
 
 // console.log(url)
 
-axios.get(url)
-.then(res => {
-    console.log(res.list[0].weather['description'])
-})
+// axios.get(url)
 // .then(res => {
 //     console.log('data')
 //     console.log(res.data)
@@ -43,3 +40,54 @@ axios.get(url)
 //     console.log(res.list[0].main['temp_min'])
 //     console.log('***************************************')
 // })
+// .then(res => {
+//   const list = res['list']
+//   for (let previsao of list){
+//     const main = previsao.main
+//     const temp_min = main.temp_min
+//     const temp_max = main['temp_max']
+//     const humidity = main.humidity
+//     console.log(`
+//       Temp min: ${temp_min},
+//       Temp max: ${temp_max},
+//       Humidity: ${humidity}
+//     `)
+//   }
+//   console.log('********************************')
+//   return res
+// })
+// .then(res => {
+//   for (let i = 0; i < res.list.length; i++){
+//     console.log(`
+//       Description: ${res.list[i].weather[0]['description']}
+//     `)  
+//   }
+//   console.log('********************************')
+//   return res
+// })
+// .then(res => {
+//   let resultado
+//   if (resultado = res.list.find(e => e.main.feels_like < 25))
+//     console.log(`Sim: ${resultado.main.feels_like}`)
+//   else
+//     console.log('Não tem')
+  
+//   console.log('********************************')
+
+// })
+
+const exercicioAsyncAwait = async () => {
+    const resultado = await axios.get(url)
+    const data = resultado.data
+    const list = data.list
+    list.forEach(e => {
+      console.log(e)
+      console.log('************************')
+    })
+    list.forEach((e) => {
+      console.log(e['main']['temp_min'] <= 27 ? e['weather'][0]['description'] : '')
+    })  
+  }
+  
+  exercicioAsyncAwait()
+  
