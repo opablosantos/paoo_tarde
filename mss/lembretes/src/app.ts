29476fabf1cd5c2bddea9087113b08f0c6878dbd
Pablo Santos <opablosantos@gmail.com>
2024-03-27 18:02:19 -0300

    test(ap01-mss): define endpoints thunder
    - define endpoint para armazenar novo lembrete
    - define endpoint para obter coleção de lembretes

--- mss/lembretes/src/app.ts
+++ mss/lembretes/src/app.ts
@@ -1,2 +1,26 @@
-let hello: string = "Hello, Typescript";
-console.log(hello)
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+interface Lembrete {
+    id: string;
+    texto: string
+}
+const lembretes: Record <string, Lembrete> = {}
+let id = '1'
+
+app.get('/lembretes', (req, res) => {
+    res.json(lembretes)
+})
+
+app.post('/lembretes', (req, res) => {
+    const { texto } = req.body
+    const lembrete = { id, texto }
+    lembretes[id] = lembrete
+    id = (+id + 1).toString()
+    res.json(lembrete)
+})
+
+const port = 4000
+app.listen(port, () => console.log(`Lembretes. Porta ${port}.`))
